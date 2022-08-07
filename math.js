afacf7238d5f4ea4449e893c796e7446a09584c4
Md Mehedi Ahsan <ahsanmehedi041@gmail.com>
2022-08-07 18:14:46 +0600

    math function added

---
+++ math.js
@@ -0,0 +1,16 @@
+/*
+  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+*/
+
+const number = 1.234;
+console.log(Math.pow(3,3));
+console.log(Math.ceil(number));
+console.log(Math.floor(number));
+console.log(Math.round(number));
+console.log(Math.round(Math.random()*100));
+
+let a=20, b=30;
+console.log(Math.abs(a-b));
+
+console.log('Max:',Math.max(2,7,3));
+console.log('Min:',Math.min(2,7,3));
